b88aa16448f1820f5ef2ff81b14b1426c5dcfdfc
Artem Vasenin <vasart169@gmail.com>
2017-09-01 10:17:01 +0300

    Fix gitprompt

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -1,1 +1,15 @@
-../.git-prompt-colors.sh
+# This is an alternative approach. Single line in git repo.
+override_git_prompt_colors() {
+  GIT_PROMPT_THEME_NAME="Custom"
+
+  GIT_PROMPT_STAGED="${Red}● "
+
+  GIT_PROMPT_COMMAND_OK="${BoldGreen}"
+  GIT_PROMPT_COMMAND_FAIL="${BoldRed}"
+
+  GIT_PROMPT_START_USER="${BoldGreen}\u@\h:${BoldYellow}${PathShort}${ResetColor}"
+  GIT_PROMPT_END_USER="_LAST_COMMAND_INDICATOR_ $ ${ResetColor}"
+  GIT_PROMPT_END_ROOT="_LAST_COMMAND_INDICATOR_ # ${ResetColor}"
+}
+
+reload_git_prompt_colors "Custom"
--- bash.sh
+++ bash.sh
@@ -1,3 +1,4 @@
 # Custom prompt
-GIT_PROMPT_THEME=Custom
-source ~/.bash-git-prompt/gitprompt.sh
+__GIT_PROMPT_DIR=~/config/bash-git-prompt
+GIT_PROMPT_THEME_FILE=~/config/.git-prompt-colors.sh
+source ~/config/bash-git-prompt/gitprompt.sh
