48735718a41c911932011c4a8825196349df2fbb
Artem Vasenin <a.vasenin@ntechlab.com>
2017-12-07 16:37:52 +0300

    Add time to git-prompt

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -8,7 +8,7 @@ override_git_prompt_colors() {
   GIT_PROMPT_COMMAND_OK="${BoldGreen}"
   GIT_PROMPT_COMMAND_FAIL="${BoldRed}"
 
-  GIT_PROMPT_START_USER="${BoldGreen}\u@\h:${BoldYellow}${PathShort}${ResetColor}"
+  GIT_PROMPT_START_USER="${White}${Time12a}${ResetColor} ${BoldGreen}\u@\h:${BoldYellow}${PathShort}${ResetColor}"
   GIT_PROMPT_END_USER="_LAST_COMMAND_INDICATOR_ $ ${ResetColor}"
   GIT_PROMPT_END_ROOT="_LAST_COMMAND_INDICATOR_ # ${ResetColor}"
 }
