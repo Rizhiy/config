1b3ad56e1fd120afa762dafd07830045b9e5f339
Artem Vasenin (Rizhiy) <vasart169@gmail.com>
2019-12-24 14:25:23 +0000

    Update profile.sh

--- profile.sh
+++ profile.sh
@@ -18,6 +18,16 @@ for configFolder in ${configFolders[@]}; do
     fi
 done
 
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
 # Set default terminal to urxvt
 export TERMINAL="urxvt"
 
