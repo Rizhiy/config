0ffe430fe10ef7f93e6e54c38bd4146fabcc1945
Artem Vasenin <a.vasenin@neurotrade.ru>
2020-01-20 11:58:52 +0300

    chore[profile.sh]: Hide sed output

--- profile.sh
+++ profile.sh
@@ -63,4 +63,4 @@ cd -
 cd "$CONFIG_FOLDER/awesome-terminal-fonts"
 ./install.sh
 cd -
-sed 's/<family>PragmataPro<\/family>/<family>FontAwesome<\/family>/g' ~/.config/fontconfig/conf.d/10-symbols.conf
+sed 's/<family>PragmataPro<\/family>/<family>FontAwesome<\/family>/g' ~/.config/fontconfig/conf.d/10-symbols.conf &>/dev/null
